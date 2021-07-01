e93ae2f08368bd2559d0c95de421031d3532a3db
takashiidobe <idobetakashi@gmail.com>
2021-07-01 09:12:04 -0400

    turn into website

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, render_template
 from recipe_scrapers import scrape_me
 from flask_cors import CORS
 
@@ -11,19 +11,11 @@ def main():
     if not URL:
         return None
     r = scrape_me(URL, wild_mode=True)
-    dt = {}
-    if r.title():
-        dt['title'] = r.title()
-    if r.total_time():
-        dt['total_time'] = r.total_time()
-    if r.yields():
-        dt['yields'] = r.yields()
-    if r.ingredients():
-        dt['ingredients'] = r.ingredients()
-    if r.instructions():
-        dt['instructions'] = r.instructions()
-    if r.image():
-        dt['image'] = r.image()
-    if r.nutrients():
-        dt['nutrients'] = r.nutrients()
-    return jsonify(dt) 
+    template = render_template('index.html', 
+        title=r.title(), 
+        total_time=r.total_time(),
+        ingredients=r.ingredients(),
+        instructions=r.instructions(),
+        image=r.image(),
+    ) 
+    return template
