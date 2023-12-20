d5d45f11bc4e33dfa3b0837b65c6b8d3f559bcb6
Takashi Idobe <mail@takashiidobe.com>
2023-12-20 12:41:22 -0500

    update dependencies and add 404

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 from flask import Flask, request, render_template
 from recipe_scrapers import scrape_me
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/", methods=['GET'])
 def main():
-    URL = request.args.get('url')
+    URL = request.args
+    if URL:
+        URL = URL.get('url')
     if not URL:
-        return None
+        return render_template('404.html')
     r = scrape_me(URL, wild_mode=True)
-    template = render_template('index.html', 
-        title=r.title(), 
+    template = render_template('index.html',
+        title=r.title(),
         total_time=r.total_time(),
         ingredients=r.ingredients(),
         instructions=r.instructions().splitlines(),
         image=r.image(),
-    ) 
+    )
     return template
